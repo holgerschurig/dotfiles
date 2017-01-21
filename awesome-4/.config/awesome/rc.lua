89830e6ea61b81bbd17ed04390588f9f1d5f0d70
Holger Schurig <holgerschurig@gmail.com>
2017-01-21 18:38:15 +0100

    awesome-4: reorder variables

--- awesome-4/.config/awesome/rc.lua
+++ awesome-4/.config/awesome/rc.lua
@@ -34,22 +34,17 @@ do
 end
 -- }}}
 
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
 beautiful.init(awful.util.get_themes_dir() .. "zenburn/theme.lua")
+-- {{{ Theme definitions
 beautiful.wallpaper = nil
+-- }}}
 
--- This is used later as the default terminal and editor to run.
+-- {{{ Variable definitions
 terminal = "urxvt"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
+-- }}}
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
