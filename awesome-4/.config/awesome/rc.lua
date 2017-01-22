41e63370d2bd7831d57d4b4a1de9a1e8747d88dc
Holger Schurig <holgerschurig@gmail.com>
2017-01-22 21:11:32 +0100

    awesome-4: move the wibox to the bottom

--- awesome-4/.config/awesome/rc.lua
+++ awesome-4/.config/awesome/rc.lua
@@ -195,7 +195,7 @@ awful.screen.connect_for_each_screen(function(s)
     s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, taglist_buttons)
 
     -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s })
+    s.mywibox = awful.wibar({ position = "bottom", screen = s })
 
     -- Add widgets to the wibox
     s.mywibox:setup {
