fd4c11558ecb37a81ccbb6aab090f9dc6984060c
Holger Schurig <holgerschurig@gmail.com>
2017-02-05 16:36:11 +0100

    awesome-4: place the first Emacs/Browser client on specific tags

--- awesome-4/.config/awesome/rules.lua
+++ awesome-4/.config/awesome/rules.lua
@@ -1,3 +1,41 @@
+-- I tried to setup a rule to put only the first emacs on tag 1, and
+-- only the first browser on tag 5, but this didn't work. So let's do
+-- this with a signal instead.
+local function manage_first(c)
+   if awesome.startup then
+	  return
+   end
+
+   local count = 0
+   local tag
+   if c.class == "Emacs" then
+	  tag = screen[1].tags[1]
+	  count = 0
+	  for _, cc in ipairs(client.get()) do
+		 if cc.class == c.class then
+			count = count + 1
+		 end
+	  end
+   end
+   if c.class == "Firefox" or c.class == "Chromium" then
+	  tag = screen[1].tags[5]
+	  print("")
+	  for _, cc in ipairs(client.get()) do
+		 print("cc.class:" .. cc.class)
+		 if cc.class == "Firefox" or cc.class == "Chromium" then
+			count = count + 1
+		 end
+	  end
+   end
+   print("Class: " .. c.class .. "  count: " .. count)
+   if count == 1 then
+   	  c:move_to_tag(tag)
+   	  c:jump_to()
+   end
+end
+client.connect_signal("manage", manage_first)
+
+
 -- Rules to apply to new clients
 awful.rules.rules = {
     -- All clients will match this rule.
@@ -11,18 +49,6 @@ awful.rules.rules = {
                      screen = awful.screen.preferred,
                      placement = awful.placement.no_overlap+awful.placement.no_offscreen,
                      size_hints_honor = false,
-     }
-    },
-
-    -- Clients on special tags
-    { rule = { class = "Emacs" },
-      properties = { tag = screen[1].tags[1],
-      switchtotag = true
-     }
-    },
-    { rule_any = { class = { "Chromium", "Firefox" } },
-      properties = { tag = screen[1].tags[5],
-      switchtotag = true
      }
     },
 
