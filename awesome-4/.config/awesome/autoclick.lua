c7ab2ee29aac0e9e41ba01a9affc4494c710e7da
Holger Schurig <holgerschurig@gmail.com>
2019-02-08 20:42:21 +0100

    awesome: updates to the autoclicker

--- awesome-4/.config/awesome/autoclick.lua
+++ awesome-4/.config/awesome/autoclick.lua
@@ -10,13 +10,16 @@ local ac     = {}
 
 local function start_stop(timer)
    if timer.started then
-	  timer:stop()
-	  return
+      -- print("stopping ...")
+      timer:stop()
+      return
    end
 
    if not mouse.current_client then
-	  return
+      -- print("not starting ...")
+      return
    end
+   -- print("starting ...")
    timer:start()
 end
 
@@ -43,9 +46,7 @@ function autoclicker_once()
 end
 
 
-autoclicker_timer = require("gears.timer") { timeout=0.08, callback= autoclicker_once }
-
-
+autoclicker_timer = require("gears.timer") { timeout=0.2, callback= autoclicker_once }
 function ac.autoclicker()
 
    local mc = mouse.coords()
@@ -62,76 +63,74 @@ end
 
 -----------------------------------------------------------------------------
 
-local function idlewizard_once()
-   -- print("Idle Wizard step " .. ac.iw)
-   if ac.iw > ac.max then
-	  -- print("reset from " .. ac.iw .. " to " .. ac.min)
-	  ac.iw = ac.min
+local clicks_idlewizard = {
+   { x = 960, y = 200 + 1*63 }, -- source
+   { x = 960, y = 200 + 2*63 }, -- source
+   { x = 960, y = 200 + 3*63 }, -- source
+   { x = 960, y = 200 + 4*63 }, -- source
+   { x = 960, y = 200 + 5*63 }, -- source
+   { x = 960, y = 200 + 6*63 }, -- source
+   { x = 960, y = 200 + 7*63 }, -- source
+   { x = 960, y = 200 + 8*63 }, -- source
+   { key = "b" },        -- buy upgrades
+   { x = 495, y = 425},  -- void source
+   { x = 584, y = 362},  -- void source
+   { x = 763, y = 348},  -- void source
+   { x = 856, y = 434},  -- void source
+   { key = "w" },        -- weapon
+   { x = 852, y = 239 }, -- buy sources
+   { x = 670, y = 443 }, -- orb
+}
+
+local clicks_wami = {
+   { x = 760, y = 690},    -- Basic Attack
+   { x = 865, y = 690},    -- Double Attack
+   { x = 990, y = 690},    -- Poison Attack
+   { x = 1110, y = 690},   -- Paralyze Attack
+   { x = 760, y = 735},    -- Block
+   { x = 865, y = 735},    -- Absorb
+   { x = 990, y = 735},    -- Counter
+   { x = 1110, y = 735},   -- Parry
+   { x = 760, y = 770},    -- Heal
+   { x = 865, y = 770},    -- Regen
+   { x = 990, y = 770},    -- Buff Stats
+   -- { x = 1110, y = 770},   -- Debuff
+}
+
+ac.clicks = clicks_wami
+
+function idleclick_once()
+   if ac.iw > table.getn(ac.clicks) then
+      ac.iw = 1
+   end
+   --  print("ac.iw " .. ac.iw)
+
+   -- send mouse click
+   local c = ac.clicks[ac.iw]
+   if c.x ~= nil then
+      mouse.coords(c, false)
+      root.fake_input("button_press",   1)
+      root.fake_input("button_release", 1)
    end
 
-   if ac.iw < 8 then
-	  -- print("buy source " .. ac.iw)
-	  mouse.coords({ x = 960, y = 200 + ac.iw * 63}, false)
-	  root.fake_input("button_press", 1)
-	  root.fake_input("button_release", 1)
-   end
-   if ac.iw == 8 then
-	  -- print "buy upgrades"
-	  root.fake_input("key_press", "b")
-	  root.fake_input("key_release", "b")
-   end
-   -- void sources
-   if ac.iw == 9 then
-	  mouse.coords({ x = 495, y = 425}, false)
-	  root.fake_input("button_press", 1)
-	  root.fake_input("button_release", 1)
-   end
-   if ac.iw == 10 then
-	  mouse.coords({ x = 584, y = 362}, false)
-	  root.fake_input("button_press", 1)
-	  root.fake_input("button_release", 1)
-   end
-   if ac.iw == 11 then
-	  mouse.coords({ x = 763, y = 348}, false)
-	  root.fake_input("button_press", 1)
-	  root.fake_input("button_release", 1)
-   end
-   if ac.iw == 12 then
-	  mouse.coords({ x = 856, y = 434}, false)
-	  root.fake_input("button_press", 1)
-	  root.fake_input("button_release", 1)
-   end
-   -- click orb
-   if ac.iw == 13 then
-	  mouse.coords({ x = 660, y = 530}, false)
-	  root.fake_input("button_press", 1)
-	  root.fake_input("button_release", 1)
+   -- send key
+   if c.key ~= nil then
+      root.fake_input("key_press",   c.key)
+      root.fake_input("key_release", c.key)
    end
 
    ac.iw = ac.iw + 1
 end
 
-idlewizard_timer = require("gears.timer") { timeout=0.2, callback= idlewizard_once }
+
+idleclick_timer = require("gears.timer") { timeout=0.07, callback=idleclick_once }
 
 function ac.idlewizard()
-   -- local mc = mouse.coords()
-   -- print("mouse at " .. mc.x .. ", " .. mc.y)
+   local mc = mouse.coords()
+   print("mouse at " .. mc.x .. ", " .. mc.y)
 
-   -- 0-7    sources
-   -- 8      upgrades
-   -- 9-12   void traps
-   -- 13     orb
-
-   ac.min = 0
-   ac.max = 12
-
-   -- ac.iw = 10
-   -- idlewizard_once()
-   -- return
-
-   ac.iw = 0
-   start_stop(idlewizard_timer)
-
+   ac.iw = 1
+   start_stop(idleclick_timer)
 end
 
 
