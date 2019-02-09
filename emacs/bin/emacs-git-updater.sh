39fcbc41b7f38b98cc884bf776e31bb67e7e76fd
Holger Schurig <holgerschurig@gmail.com>
2019-02-09 10:16:03 +0100

    emacs: misc updates to the emacs-git-puller

--- emacs/bin/emacs-git-updater.sh
+++ emacs/bin/emacs-git-updater.sh
@@ -43,19 +43,35 @@ show_last()
 {
 	local D
 	local DATE
+	declare -a KEYS
+	declare -lA DATES
+	declare -lA URLS
+
+	if [[ $1 == elpa* ]]; then
+		KEYS=("-k" "5,5" "-k" "3,3")  # sort by date, then by URL
+	else
+		KEYS=("-k" "6,6" "-k" "4,4")  # sort by dtae, then by name
+	fi
 
 	for i in ${1}*/.git; do
 		# We get this weird string when we don't find a match
 		test $i = "${1}*/.git" && break
 		D=${i%/.git}
 
-		# Use a ( so that even in case of a failure we end up in our directory
-		(
-			cd $D
-			DATE=`git show --format=format:%ai | head -n1`
-			echo "$DATE  $D"
-		)
+		DATE=`cd $D; git show --format=format:%ai | head -n1 | cut -d' ' -f1`
+		DATES[$D]="$DATE"
+		URLS[$D]=`cd $D; git config remote.origin.url`
+		#echo "$DATE $D"
 	done
+	(
+		for D in ${!DATES[@]} ; do
+			if [[ $D == elpa-* ]] ; then
+				printf "\tclone elpa %-50s # %s\n" "${URLS[$D]}" "${DATES[$D]}"
+			else
+				printf "\tclone %-12s %-50s %-12s # %s\n" "${D/-*/}" "${URLS[$D]}" "${D/*-/}" "${DATES[$D]}"
+			fi
+		done
+	) | sort -r ${KEYS[*]}
 }
 
 
@@ -83,43 +99,33 @@ clone()
 
 clone_conf()
 {
-	# Changed in 2017-01
-	clone confevilivy  https://github.com/redguardtoo/emacs.d               redguardtoo
-	clone confivy      https://github.com/abo-abo/oremacs.git               aboabo
-	clone confevilhelm https://github.com/jwiegley/dot-emacs.git            jwiegley
-	clone confhelm     https://github.com/angrybacon/dotemacs.git           angrybacon
-	clone confevilhelm https://github.com/dakrone/eos.git                   eos
-	clone confivy      https://github.com/kaushalmodi/.emacs.d.git          kaushalmodi
-	clone confhelm     https://github.com/sachac/.emacs.d.git               sachac
-	clone confhelm     https://github.com/bodil/ohai-emacs.git              ohai
-	#clone confhelm     https://github.com/mordocai/emacs.d.git              mordocai
-	clone confhelm     https://github.com/sri/dotfiles.git                  sri
-	clone confhelm     https://github.com/bbatsov/prelude.git               prelude
-	clone confivy      https://github.com/skeeto/.emacs.d.git               skeeto
-	clone confivy      https://github.com/purcell/emacs.d.git               purcell
-	clone conf         https://github.com/novoid/Memacs.git                 memacs
-
-	# Changed in 2016-12
-	clone conf         https://github.com/skybert/my-little-friends.git     skybert
-	clone confhelm     https://github.com/sigma/dotemacs.git                sigma
-	clone confhelm     https://github.com/Fuco1/.emacs.d.git                fuco1
-	clone conf         https://github.com/chrisdone/chrisdone-emacs.git     chrisdone
-
-	# Changed in 2016-11
-	clone conf         https://github.com/novoid/dot-emacs.git              voit
-	clone confevilhelm https://github.com/milkypostman/dotemacs.git         milkypostman
-	clone confivy      https://github.com/lunaryorn/.emacs.d.git            lunaryorn
-	clone confhelm     https://github.com/tuhdo/emacs-c-ide-demo.git        tuhdo
-
-	# Changed in 2016-10
-	clone confhelm     https://github.com/bashrc/emacs.git                  bashrc
-	clone conf         https://github.com/jd/emacs.d.git                    danjou
-	clone conf         https://github.com/higham/dot-emacs.git              higham
-
-	# Not changed for a while ...
-	clone oldhelm      https://github.com/jkitchin/jmax.git                 kitchin
-	clone old          https://github.com/magnars/.emacs.d.git              magnars
-	clone old          https://github.com/grettke/home.git                  grettke
+	clone confhelm     https://github.com/howardabrams/dot-files          howardabrahms
+	clone confivy      https://github.com/purcell/emacs.d.git             purcell      # 2017-10-07
+	clone conf         https://github.com/skybert/my-little-friends.git   skybert      # 2017-10-06
+	clone confevilivy  https://github.com/redguardtoo/emacs.d             redguardtoo  # 2017-10-06
+	clone confivy      https://github.com/skeeto/.emacs.d.git             skeeto       # 2017-10-05
+	clone confhelm     https://github.com/fuco1/.emacs.d.git              fuco1        # 2017-10-05
+	clone confivy      https://github.com/abo-abo/oremacs.git             aboabo       # 2017-10-05
+	clone confivy      https://github.com/kaushalmodi/.emacs.d.git        kaushalmodi  # 2017-10-04
+	clone conf         https://github.com/chrisdone/chrisdone-emacs.git   chrisdone    # 2017-10-01
+	clone conf         https://github.com/magnars/.emacs.d.git            magnars      # 2017-09-28
+	clone confevilhelm https://github.com/dakrone/eos.git                 eos          # 2017-09-26
+	clone conf         https://github.com/novoid/dot-emacs.git            voit         # 2017-09-23
+	clone confhelm     https://github.com/bashrc/emacs.git                bashrc       # 2017-09-20
+	clone conf         https://github.com/novoid/memacs.git               memacs       # 2017-09-16
+	clone confhelm     https://github.com/bbatsov/prelude.git             prelude      # 2017-09-12
+	clone confhelm     https://github.com/angrybacon/dotemacs.git         angrybacon   # 2017-08-27
+	clone conf         https://github.com/jd/emacs.d.git                  danjou       # 2017-08-22
+	clone confhelm     https://github.com/sachac/.emacs.d.git             sachac       # 2017-08-04
+	clone confevilhelm https://github.com/jwiegley/dot-emacs.git          jwiegley     # 2017-08-01
+	clone conf         https://github.com/higham/dot-emacs.git            higham       # 2017-07-31
+	clone confhelm     https://github.com/sri/dotfiles.git                sri          # 2017-07-25
+	clone confhelm     https://github.com/bodil/ohai-emacs.git            ohai         # 2017-07-05
+	clone confivy      https://github.com/lunaryorn/.emacs.d.git          lunaryorn    # 2017-06-30
+	clone confevilhelm https://github.com/milkypostman/dotemacs.git       milkypostman # 2017-02-07
+	clone confhelm     https://github.com/sigma/dotemacs.git              sigma        # 2016-12-21
+	clone confhelm     https://github.com/tuhdo/emacs-c-ide-demo.git      tuhdo        # 2016-11-08
+	clone conf         https://github.com/grettke/home.git                grettke      # 2016-04-25
 }
 
 
@@ -127,51 +133,35 @@ clone_elpa()
 {
 	# You can find the URLs via http://melpa.milkbox.net/#/
 
-
-	# Changed in 2017:
-	clone elpa https://github.com/jorgenschaefer/circe.git
-	clone elpa https://github.com/flycheck/flycheck.git
-	clone elpa https://github.com/magit/magit.git
-	clone elpa https://github.com/emacs-helm/helm.git
-	clone elpa https://github.com/abo-abo/hydra.git
-	clone elpa https://github.com/rust-lang/rust-mode.git
-	clone elpa https://github.com/fxbois/web-mode.git
-	clone elpa https://github.com/bbatsov/zenburn-emacs
-
-	# Changed in 2016-12
-	clone elpa https://github.com/jwiegley/use-package.git
-	clone elpa https://github.com/jacktasia/dumb-jump.git
-	clone elpa https://github.com/skeeto/elfeed.git
-	clone elpa https://github.com/xiongtx/eros.git
-	clone elpa https://github.com/dominikh/go-mode.el.git
-	clone elpa https://github.com/hniksic/emacs-htmlize.git
-	clone elpa https://github.com/mooz/js2-mode.git
-	clone elpa https://github.com/hlissner/emacs-pug-mode.git
-	clone elpa https://github.com/justbur/emacs-which-key.git
-	clone elpa https://github.com/magit/with-editor.git
-
-	# Changed in 2016-11
-	# async
-	# dash
-	clone elpa https://github.com/magnars/expand-region.el.git
-	clone elpa https://github.com/pidu/git-timemachine.git
-	# lua-mode
-	clone elpa https://github.com/Malabarba/paradox.git
-	clone elpa https://github.com/milkypostman/powerline.git
-	# toml-mode
-	clone elpa https://github.com/jinzhu/zeal-at-point.git
-
-	# Changed in 2016-10
-	# d-mode
-	clone elpa https://github.com/rejeep/f.el.git
-	clone elpa https://github.com/emacsfodder/kurecolor.git
-	clone elpa https://github.com/emacsmirror/undo-tree.git
-
-	# Changed in 2016-09
-	clone elpa https://github.com/cute-jumper/avy-zap.git
-	clone elpa https://github.com/emacs-helm/helm-descbinds.git
-	clone elpa https://github.com/pronobis/helm-flyspell.git
-	clone elpa https://github.com/magnars/s.el.git
+	clone elpa https://github.com/magit/magit.git                 # 2017-10-07
+	clone elpa https://github.com/flycheck/flycheck.git           # 2017-10-07
+	clone elpa https://github.com/abo-abo/swiper                  # 2017-10-07
+	clone elpa https://github.com/magit/with-editor.git           # 2017-10-06
+	clone elpa https://github.com/fxbois/web-mode.git             # 2017-10-02
+	clone elpa https://github.com/skeeto/elfeed.git               # 2017-10-01
+	clone elpa https://github.com/mooz/js2-mode.git               # 2017-09-30
+	clone elpa https://github.com/hlissner/emacs-pug-mode.git     # 2017-09-30
+	clone elpa https://github.com/jwiegley/use-package.git        # 2017-09-25
+	clone elpa https://github.com/abo-abo/hydra.git               # 2017-09-25
+	clone elpa https://github.com/darwinawardwinner/amx           # 2017-09-23
+	clone elpa https://github.com/magnars/s.el.git                # 2017-09-22
+	clone elpa https://github.com/rust-lang/rust-mode.git         # 2017-09-20
+	clone elpa https://github.com/jacktasia/dumb-jump.git         # 2017-09-18
+	clone elpa https://github.com/malabarba/paradox.git           # 2017-09-15
+	clone elpa https://github.com/abo-abo/avy                     # 2017-08-19
+	clone elpa https://github.com/justbur/emacs-which-key.git     # 2017-08-17
+	clone elpa https://github.com/emacsfodder/kurecolor.git       # 2017-08-08
+	clone elpa https://github.com/dominikh/go-mode.el.git         # 2017-07-26
+	clone elpa https://github.com/hniksic/emacs-htmlize.git       # 2017-07-17
+	clone elpa https://github.com/milkypostman/powerline.git      # 2017-07-08
+	clone elpa https://github.com/emacsmirror/undo-tree.git       # 2017-07-06
+	clone elpa https://github.com/magnars/expand-region.el.git    # 2017-05-14
+	clone elpa https://github.com/bbatsov/zenburn-emacs           # 2017-05-11
+	clone elpa https://github.com/jinzhu/zeal-at-point.git        # 2017-04-28
+	clone elpa https://github.com/rejeep/f.el.git                 # 2017-04-04
+	clone elpa https://github.com/pidu/git-timemachine.git        # 2017-03-25
+	clone elpa https://github.com/xiongtx/eros.git                # 2016-12-21
+	clone elpa https://github.com/cute-jumper/avy-zap.git         # 2016-09-21
 }
 
 
